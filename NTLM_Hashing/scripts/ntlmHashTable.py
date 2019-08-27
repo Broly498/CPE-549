b16e7a516f91b43d8157d7b47b4c7c84306252d3
Broly498 <34816914+Broly498@users.noreply.github.com>
2019-08-27 15:44:13 -0500

    Adding NTLM Hashing Project.

---
+++ NTLM_Hashing/scripts/ntlmHashTable.py
@@ -0,0 +1,99 @@
+'''This program will create a text file containing a table of 
+   plain-text passwords as well as their hashed counterparts.
+   The hashing algorithm used is that of NT Lan Manager (NTLM).
+   
+   The program is designed designed to parse list files containing collections of plain-text passwords.
+   Passwords that are contained inside of the list file should be new-line delimited (\n).
+   The list file may also contain comments which are denoted by a pound sign (#).
+   
+   ------------------------------------------------------------
+   ------------------- Begin sampleFile.lst -------------------
+   ------------------------------------------------------------
+   #comment1\n
+   #comment2\n
+   #comment3\n
+   password1\n
+   password2\n
+   password3\n
+   ------------------------------------------------------------
+   -------------------- End sampleFile.lst --------------------
+   ------------------------------------------------------------
+
+   The program will create a directory named "./outputs".
+   The directory will contain files that have been created by the program.
+   Each file will be prepended with a time-stamp in the following format: yyyymmddhhmmss_inputFile.lst.
+   The output file will contain a list of the original plain-text
+   passwords as well as their hashed counterparts.
+   
+   ------------------------------------------------------------
+   ------------------- Begin outputFile.lst -------------------
+   ------------------------------------------------------------
+   password1 : hash1\n
+   password2 : hash2\n
+   password3 : hash3\n
+   ------------------------------------------------------------
+   -------------------- End outputFile.lst --------------------
+   ------------------------------------------------------------
+
+   Expected Command-Line Arguments:
+   Arg1: <inputFile>
+   Arg2: <outputDirectory>
+'''
+
+import hashlib
+import os
+import sys
+import datetime
+
+print("Beginning NTLM Hashing Program...")
+
+#The first command-line argument is always the name of the script that is being called
+if len(sys.argv) is not 3:
+    print("Invalid Number of Command-Line Arguments Were Specified: %d" % (len(sys.argv) - 1))
+    print("\nExpected Command-Line Arguments:")
+    print("Argv 1: <inputFile>")
+    print("Argv 2: <outputDirectory>")
+    sys.exit("\nTerminating Program.")
+
+inputFile = sys.argv[1]
+outputDirectory = sys.argv[2]
+timeStamp = datetime.datetime.now()
+
+outputFile = outputDirectory + "/%d%02d%02d%02d%02d%02d_hashTable.lst" % \
+    (timeStamp.year, timeStamp.month, timeStamp.day, \
+    timeStamp.hour, timeStamp.minute, timeStamp.second)
+
+#Validate input file
+if not os.path.exists(inputFile):
+    sys.exit("Input File Was Not Specified Argv 1: " + inputFile)
+
+#Validate output directory
+if not os.path.exists(outputDirectory):
+    os.makedirs(outputDirectory)
+
+print("Parsing Input File: " + inputFile)
+
+plainTextPasswordList = []
+ntlmPasswordList = []
+
+#Open new context and open wordList File
+with open(inputFile, 'r') as file:
+    for line in file:
+        #Ignore any lines that are comments
+        if not line.startswith('#'):
+            #Extract plain text password
+            plainTextPassword = line.splitlines()[0]
+            plainTextPasswordList.append(plainTextPassword)
+
+            #Create NTLM password
+            ntlmPassword = hashlib.new('md4', plainTextPassword.encode('utf_16_LE')).hexdigest()
+            ntlmPasswordList.append(ntlmPassword)
+
+#Open new context and create hashtable file
+with open(outputFile, 'w+') as file:
+    for i in range(len(ntlmPasswordList)):
+        file.write("%s : %s\n" % (plainTextPasswordList[i], ntlmPasswordList[i]))
+
+print("Output Hash Table File Was Created: " + outputFile)
+
+print("NTLM Hashing Program Concluded...")
